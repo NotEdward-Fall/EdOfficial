139152595de6fb7176b02a79f24a3340fd438660
Edward Peh <edwardpeh001@gmail.com>
2025-02-23 13:26:07 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {navMenu.classList.toggle('hide');})
